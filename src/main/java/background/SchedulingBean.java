8c2a1a43ba98d11e2d0751263b3f37dca5e97fb5
James H Britton <jhb15@aber.ac.uk>
2018-11-08 21:01:49 +0000

    Added an EJB for Startup Scheduling
    This is only an EJB with methods stubs at the minuet that print messages.

---
+++ src/main/java/background/SchedulingBean.java
@@ -0,0 +1,49 @@
+package background;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.ejb.Schedule;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+
+/**
+ * Scheduling EJB for kicking of the background tasks that will run once the application has started up.
+ */
+@Startup
+@Singleton
+public class SchedulingBean {
+
+    /**
+     * This method is ran once the EJB is created.
+     */
+    @PostConstruct
+    public void atStartup() {
+        System.out.println("Scheduling EJB Initialised!");
+    }
+
+    /**
+     * This method is ran every hour.
+     */
+    @Schedule(hour = "*/1", minute = "0", second = "0", persistent = false)
+    public void getFitbitData() {
+        System.out.println("Method stub for retrieving Fitbit Data!");
+        //TODO implement
+    }
+
+    /**
+     * This method is ran every half hour.
+     */
+    @Schedule(hour= "*", minute = "*/30", second = "0", persistent = false)
+    public void updateActivityMappings() {
+        System.out.println("Method stub for Update Activity Mappings!");
+        //TODO implement
+    }
+
+    /**
+     * This method is ran once the EJB is destroyed.
+     */
+    @PreDestroy
+    public void atEnd() {
+        System.out.println("Destroying Scheduling EJB");
+    }
+}
