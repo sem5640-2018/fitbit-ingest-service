54887327ef96569e1aa46a66ca4927464a5740ec
Jack Thomson <Jackathomson@outlook.com>
2018-11-14 14:18:38 +0000

    Add initial framework for polling.
    This commit also adds a new method to retrieve all the tokens from the database.

--- src/main/java/datacollection/DataCheckThread.java
+++ src/main/java/datacollection/DataCheckThread.java
@@ -2,17 +2,26 @@ package datacollection;
 
 import persistence.TokenMap;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class DataCheckThread implements Runnable {
-
     private ConcurrentLinkedQueue<TokenMap> input;
     private ConcurrentLinkedQueue<String> output;
+    private String address;
 
     DataCheckThread(ConcurrentLinkedQueue<TokenMap> input, ConcurrentLinkedQueue<String> out) {
         // Create Shallow copy to the global linked queue
         this.input = input;
         this.output = out;
+
+        // Needed for API requests with FitBit
+        Date date = new Date();
+        String textDate = new SimpleDateFormat("yyyy-MM-dd").format(date);
+
+        this.address = "https://api.fitbit.com/1/user/-/activities/date/" + textDate + ".json";
+
     }
 
 
@@ -34,11 +43,25 @@ public class DataCheckThread implements Runnable {
      * @return JSON Array with activity data
      */
     private void requestActivityData(TokenMap tokenMap) {
+        /*final OAuth2AccessToken oauth2AccessToken = new FitBitOAuth2AccessToken(tokenMap.getAccessToken());
+
+        if (!(oauth2AccessToken instanceof FitBitOAuth2AccessToken)) {
+            return;
+        }
+
+        final FitBitOAuth2AccessToken accessToken = (FitBitOAuth2AccessToken) oauth2AccessToken;
+
+        final OAuthRequest request = new OAuthRequest(Verb.GET,
+                String.format(this.address, accessToken.getUserId()));
+        request.addHeader("x-li-format", "json");
+
+        // TODO with a global service to access
+        //service.signRequest(accessToken, request);
 
         //TODO will send of API request for activity data
         String s = "some JSON to be parsed";
 
-        output.add(s);
+*/
     }
 
 }
