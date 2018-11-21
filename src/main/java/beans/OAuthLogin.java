419ff8f7b08eccaea90f5c031ded6278537f47ec
Jack Thomson <Jackathomson@outlook.com>
2018-11-21 13:00:11 +0000

    Convert Facelet to Servlet

--- src/main/java/beans/OAuthLogin.java
+++ src/main/java/beans/OAuthLogin.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "Login", urlPatterns = { "/LoginPage" } )
+@WebServlet(name = "Login", urlPatterns = { "/" } )
 public class OAuthLogin extends HttpServlet{
 
     @Inject
