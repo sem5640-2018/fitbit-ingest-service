07a5e076983750db5e396b2357903886dc75a3db
James H Britton <jhb15@aber.ac.uk>
2018-11-22 23:55:01 +0000

    Updated audit helper with scribe java code for sending requests to the Glados service.

--- src/main/java/logging/AuditHelper.java
+++ src/main/java/logging/AuditHelper.java
@@ -1,21 +1,31 @@
 package logging;
 
+import beans.OAuthBean;
+import com.github.scribejava.core.model.OAuthRequest;
+import com.github.scribejava.core.model.Response;
+import com.github.scribejava.core.model.Verb;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+
+import javax.ejb.EJB;
+import java.io.IOException;
 import java.util.Date;
+import java.util.concurrent.ExecutionException;
 
 /**
  * Audit Helper for generating audit loggs to send off to the glados service.
- * @Author James H Britton
- * @Author 'jhb15@aber.ac.uk'
- * @Version 0.1
+ * @author James H Britton
+ * @author 'jhb15@aber.ac.uk'
+ * @version  0.1
  */
 public class AuditHelper {
 
+    @EJB
+    private OAuthBean oAuthBean;
+
     private static String destUrl = "glados.aberfitness.biz";
     private static String addAuditUrl = "/audit/new";
-    private static String serviceName = "fitbit-ingest-service"; //TODO needs to be an envirment variable probably.
-    private static String currentUser = "test_user"; //TODO needs to come from logged in user.
+    private static String serviceName = "unknown";
 
     private Gson gson;
 
@@ -27,22 +37,40 @@ public class AuditHelper {
         serviceName = System.getenv("SERVICE_NAME");
     }
 
+
     /**
      * Sends an Audit Log to GLADOS using the API.
      * @param severity Audit Severity
      * @param msg Audit Message
+     * @param currentUser user id of logged in user
+     * @param accessToken access token for current user
+     * @return 201 created, 401 unauthorised, 500 server error, 999 Exception Thrown
      */
-    public int sendAudit(String severity, String msg) {
+    public int sendAudit(String severity, String msg, String currentUser, String accessToken) {
+        try {
 
-        String content = "[" + severity + "]: " + msg;
-        AuditObj audObj = new AuditObj(content, serviceName, new Date(), currentUser);
-        String outJson = gson.toJson(audObj);
+            String content = "[" + severity + "]: " + msg;
+            AuditObj audObj = new AuditObj(content, serviceName, new Date(), currentUser);
+            String outJson = gson.toJson(audObj);
 
-        System.out.println("Audit JSON to be sent to GLADOS: " + outJson);
-        //TODO implement API connection!
-        destUrl.length(); //TODO Remove
-        addAuditUrl.length(); //TODO Remove
-        return 501; //Unimplemented.
+            System.out.println("Audit JSON to be sent to GLADOS: " + outJson);
+            final OAuthRequest request = new OAuthRequest(Verb.POST, destUrl + addAuditUrl);
+            request.addHeader("Content-Type", "application/json;charset=UTF-8");
+            request.addBodyParameter("payload", outJson);
+            oAuthBean.getAberfitnessService().signRequest(accessToken, request);
+
+            final Response response = oAuthBean.getAberfitnessService().execute(request);
+
+            return response.getCode();
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return 999;
     }
 }
 
@@ -61,22 +89,6 @@ class AuditObj {
         this.serviceName = serviceName;
         this.timestamp = timestamp;
         this.userId = userId;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public String getServiceName() {
-        return serviceName;
-    }
-
-    public Date getTimestamp() {
-        return timestamp;
-    }
-
-    public String getUserId() {
-        return userId;
     }
 
     @Override
