779741e842f5c12895157b3c31da42dedb5c2a59
Jack Thomson <Jackathomson@outlook.com>
2018-11-30 15:21:11 +0000

    Initial prompt implementation

---
+++ src/main/java/serverlets/Prompt.java
@@ -0,0 +1,71 @@
+package serverlets;
+
+import beans.EnvironmentVariableClass;
+import beans.OAuthBean;
+import datacollection.*;
+import persistence.TokenMap;
+import scribe_java.GatekeeperLogin;
+
+import javax.ejb.EJB;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+@WebServlet(name = "Prompt", urlPatterns = {"/Prompt", "/GetFitbit"})
+public class Prompt extends HttpServlet {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @EJB
+    OAuthBean oAuthBean;
+
+    @EJB
+    GatekeeperLogin gatekeeperLogin;
+
+    private final FitbitDataCollector collector = new FitbitDataCollector(oAuthBean);
+    private final FitbitDataConverter converter = new FitbitDataConverter();
+    private final FitbitDataProcessor processor = new FitbitDataProcessor();
+    private final ActivityMapLoading loading = new ActivityMapLoading();
+
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+
+        String state = request.getParameter("state");
+        String userID = null;
+
+        if (state != null &&state.equals("gateAccess")){
+            userID = gatekeeperLogin.getUser_id();
+        }
+
+        if(userID == null) {
+            gatekeeperLogin.redirectToGatekeeper(response, EnvironmentVariableClass.getFitbitIngestPromptUrl(), "gateAccess");
+            return;
+        }
+
+        TokenMap userToken = TokenMap.getTokenMap(em, userID);
+
+        // Retrieve all the JSON strings needed for processing
+        ConcurrentLinkedQueue<ProcessedData> data = collector.getAllUsersInfo(new TokenMap[]{userToken});
+
+        // Convert all our strings to usable objects
+        data = converter.convertActivityData(data);
+
+        // Process all out data
+        processor.ProcessData(data);
+
+        response.sendRedirect(EnvironmentVariableClass.getHeathDataRepoUrl());
+    }
+
+}
