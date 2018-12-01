8d3fd31f6bc36b1201a5c523e431ccf1ab5cf368
Jack Thomson <Jackathomson@outlook.com>
2018-12-01 12:46:16 +0000

    Define https protocol for requests.

--- src/main/java/datacollection/ActivityMapLoading.java
+++ src/main/java/datacollection/ActivityMapLoading.java
@@ -29,7 +29,7 @@ public class ActivityMapLoading {
 
     private static String getHTML(String urlToRead) throws Exception {
         StringBuilder result = new StringBuilder();
-        URL url = new URL(urlToRead);
+        URL url = new URL("https://" + urlToRead);
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
         conn.setRequestMethod("GET");
         BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
