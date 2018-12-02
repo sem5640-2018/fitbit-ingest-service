1f45ad592c67e1a9fcd71a31a752a3681e78693c
James H Britton <jhb15@aber.ac.uk>
2018-12-02 22:35:07 +0000

    Removed hardcode 'http://' string from these two classes. The enviroment var they use should already contain the https prefix.

--- src/main/java/datacollection/ActivityMapLoading.java
+++ src/main/java/datacollection/ActivityMapLoading.java
@@ -27,7 +27,7 @@ public class ActivityMapLoading {
 
     private static String getHTML(String urlToRead) throws Exception {
         StringBuilder result = new StringBuilder();
-        URL url = new URL("https://" + urlToRead);
+        URL url = new URL(urlToRead);
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
         conn.setRequestMethod("GET");
         BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
