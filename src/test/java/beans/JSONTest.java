244b8ef9cb3a0a231a3eee98498f9eec8ee0d41f
James H Britton <jhb15@aber.ac.uk>
2018-12-12 13:14:30 +0000

    Tests now run and pass if enviroment variables are set.

--- src/test/java/beans/JSONTest.java
+++ src/test/java/beans/JSONTest.java
@@ -1,11 +1,22 @@
 package beans;
 
 import com.google.gson.Gson;
+import datacollection.FitbitDataProcessor;
+import datacollection.ProcessedData;
 import datacollection.mappings.FitBitJSON;
 import org.junit.Assert;
 import org.junit.Test;
+import org.mockito.Mock;
+import persistence.TokenMap;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class JSONTest {
+
+    @Mock
+    ActivityMappingBean activityMappingBean;
 
     private String sampleJSON = "{\"activities\":[{\"activityId\":51007,\"activityParentId\":90019,\"calories\":230,\"description\":\"7mph\",\"distance\":2.04,\"duration\":1097053,\"hasStartTime\":true,\"isFavorite\":true,\"logId\":1154701,\"name\":\"Treadmill, 0% Incline\",\"startTime\":\"00:25\",\"steps\":3783}],\"goals\":{\"caloriesOut\":2826,\"distance\":8.05,\"floors\":150,\"steps\":10000},\"summary\":{\"activityCalories\":230,\"caloriesBMR\":1913,\"caloriesOut\":2143,\"distances\":[{\"activity\":\"tracker\",\"distance\":1.32},{\"activity\":\"loggedActivities\",\"distance\":0},{\"activity\":\"total\",\"distance\":1.32},{\"activity\":\"veryActive\",\"distance\":0.51},{\"activity\":\"moderatelyActive\",\"distance\":0.51},{\"activity\":\"lightlyActive\",\"distance\":0.51},{\"activity\":\"sedentaryActive\",\"distance\":0.51},{\"activity\":\"Treadmill, 0% Incline\",\"distance\":3.28}],\"elevation\":48.77,\"fairlyActiveMinutes\":0,\"floors\":16,\"lightlyActiveMinutes\":0,\"marginalCalories\":200,\"sedentaryMinutes\":1166,\"steps\":0,\"veryActiveMinutes\":0}}";
 
@@ -22,7 +33,7 @@ public class JSONTest {
     }
 
 
-    /*@Test
+    @Test
     public void TestProcess() throws InterruptedException {
         TokenMap tm = new TokenMap();
         tm.setUserID("");
@@ -46,12 +57,12 @@ public class JSONTest {
             queue.add(data);
         }
 
-        FitbitDataProcessor processor = new FitbitDataProcessor();
+        FitbitDataProcessor processor = new FitbitDataProcessor(activityMappingBean);
 
         processor.ProcessSynchronous(queue);
-    }*/
+    }
 
-    /*@Test
+    @Test
     public void TestConcurrency() {
         TokenMap tm = new TokenMap();
         tm.setUserID("");
@@ -69,7 +80,7 @@ public class JSONTest {
         for(int i = 0; i < 10; i++) {
             data.addProcessedActivity(fitBitJSON);
         }
-        FitbitDataProcessor processor = new FitbitDataProcessor();
+        FitbitDataProcessor processor = new FitbitDataProcessor(activityMappingBean);
 
         ConcurrentLinkedQueue<ProcessedData> queue = new ConcurrentLinkedQueue<ProcessedData>();
         for(int i = 0; i < 100000; i++) {
@@ -77,5 +88,5 @@ public class JSONTest {
         }
 
         processor.ProcessData(queue);
-    }*/
+    }
 }
